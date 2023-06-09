b38b91d0b655c13ec24c56a0bc24a127c0bad8fc
SithumGunawardhana <sithumthilankagunawardhana@gmail.com>
2023-06-09 14:12:14 +0530

    set ui

---
+++ ChatApp/src/controller/ClientFormController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ClientFormController {
+    public Label lblName;
+    public JFXTextArea txtArea;
+    public TextField txtEnter;
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnImgOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnImogiOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ ChatApp/src/controller/LoginFormController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+    public TextField txtUserName;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+}
